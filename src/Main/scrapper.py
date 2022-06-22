8f164fb28cebf43132fbcd8c9f086dd56dcd1d71
Pastor-git <maciej.pastor@gmail.com>
2022-06-22 12:48:45 +0200

    cleaning

--- src/Main/scrapper.py
+++ src/Main/scrapper.py
@@ -34,21 +34,6 @@ while step < 8:
 frame.to_excel(r'C:\Python Projects\Scrapper\src\OutputExcell\OutputData.xlsx')
 
 #
-# website = frame.iat[0,2]
-#
-# print(website)
-# # ------------------------------------------------
-# driver = webdriver.Chrome(executable_path="C:\webdrivers\chromedriver.exe")
-# driver.get(website)
-#
-# mail = driver.find_element(By.XPATH, "//*[contains(text(),'@')]")
-#
-# print(mail)
-#
-# # mail = "text"
-#
-# frame.at[0,'email'] = mail
-#
 # print(frame)
 # # akceptuj wszystko
 # # /html/body/div[2]/div[2]/div[3]/span/div/div/div/div[3]/div[1]/button[2]/div
@@ -56,9 +41,3 @@ frame.to_excel(r'C:\Python Projects\Scrapper\src\OutputExcell\OutputData.xlsx')
 # # wyszukiwanie
 # # /html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input
 #
-# # ZAPIS
-# frame.to_excel(r'C:\Python Projects\Scrapper\src\OutputExcell\OutputData.xlsx')
-#
-# # writer = ExcelWriter
-# driver.close()
-# driver.quit()
