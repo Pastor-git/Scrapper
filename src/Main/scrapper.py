72d650546abb44a54e356a0e3cd6e0b9bcabe0ce
Pastor-git <maciej.pastor@gmail.com>
2022-11-03 14:10:15 +0100

    update with "window" step

--- src/Main/scrapper.py
+++ src/Main/scrapper.py
@@ -3,6 +3,10 @@ from selenium import webdriver
 import pandas as panda
 import numpy as nump
 from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
 
 excellinput = panda.read_excel ("C:\Python Projects\Scrapper\src\InputExcell\InputData.xlsx")
 print (excellinput)
@@ -13,19 +17,33 @@ print(row_number)
 
 # PĘTLA
 step = 0
-while step < 8:
+while step < 9:
 
-    website = frame.iat[step,2]
-    driver = webdriver.Chrome(executable_path="C:\webdrivers\chromedriver.exe")
-    driver.get(website)
+    website = frame.iat[step, 2]
+    # driver = webdriver.Chrome(executable_path="C:\webdrivers\chromedriver.exe")
+    # driver.get(website)
+
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.get("https://www.google.com")
+
     # BRAK PRZYPISANIA
     TEST = driver.find_element(By.XPATH, "//*[contains(text(),'@')]")
     print("SELEMIUN ELEMENT: ", TEST)
     mail = str(TEST)
-    frame.at[step, 'email'] = mail
+    element = driver.find_element(By.XPATH, "//*[contains(text(),'@')]").get_attribute("textContent")
+    frame.at[step, 'email'] = element
+    print(frame)
+    TEST2 = driver.find_elements(By.XPATH, "//*[contains(text(),'window')]")
+    if TEST2 is not None:
+        frame.at[step, 'window'] = "tak"
+    else:
+        frame.at[step, 'window'] = "nie"
     print(frame)
     driver.close()
     driver.quit()
+
+
+
     # instrukcja
     step = step+1
 
